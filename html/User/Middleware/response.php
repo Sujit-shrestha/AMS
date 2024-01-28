6b1f049cbcaf04abdddab1989a4fd635198cc87c
sujit <sujitshrestha0004@gmail.com>
2024-01-28 17:18:16 +0545

    rmeoved header response: gave 500 error

--- html/User/Middleware/response.php
+++ html/User/Middleware/response.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
 namespace Middleware;
 
-class Response {
+class Response
+{
 
-  public static function respondWithJson($data , $status=200){
-    header('Content-type : application/json');
+  public static function respondWithJson($data, $status = 200)
+  {
+    // header('Content-type : application/json');
     http_response_code($status);
     unset($data["statusCode"]);
-    echo json_encode($data , JSON_PRETTY_PRINT);
+    echo json_encode($data, JSON_PRETTY_PRINT);
   }
 }
 
